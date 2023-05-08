7e111ce2f84cf344a3013dac18427bdb4dd08ced
Bruno Camargo <brunovieiracamargo@gmail.com>
2023-05-08 09:29:04 -0300

    feat: return total of Messi goals in the three major leagues.

---
+++ src/MessiGoals.java
@@ -0,0 +1,9 @@
+public class MessiGoals {
+    public static void main(String[] args) {
+        System.out.println(goals(43,10,5));
+    }
+
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
